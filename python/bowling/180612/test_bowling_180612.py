2d15d1780c9612465892371d81de04f20abde2b6
t.oto <t.oto12.dev@gmail.com>
2018-06-12 13:25:27 +0900

    practice python bowling

---
+++ python/bowling/180612/test_bowling_180612.py
@@ -0,0 +1,26 @@
+import unittest
+
+from bowling_180612 import BowlingGame
+
+class BowlingGameTests(unittest.TestCase):
+
+    def setUp(self):
+        self.game = BowlingGame()
+
+    def repeatRolls(self, frameCount, pins1st, pins2nd):
+        for frame in range(frameCount):
+            self.game.roll(pins1st)
+            if pins1st != 10:
+                self.game.roll(pins2nd)
+
+    def test_allStrike(self):
+        self.repeatRolls(12, 10, 0)
+        self.assertEqual(300, self.game.score())
+
+    def test_allSpare(self):
+        self.repeatRolls(11, 5, 5)
+        self.assertEqual(150, self.game.score())
+
+    def test_allNines(self):
+        self.repeatRolls(10, 9, 0)
+        self.assertEqual(90, self.game.score())
