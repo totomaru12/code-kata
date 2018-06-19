e0efc88d28382184783a3b90ccf380f27f5c2e3a
t.oto <t.oto12.dev@gmail.com>
2018-06-19 19:16:15 +0900

    replace raw value to constant

--- ruby/bowling_180619.rb
+++ ruby/bowling_180619.rb
@@ -1,5 +1,9 @@
 class BowlingGame
 
+    SCORE_MAX_IN_FRAME = 10
+    NORMAL_FRAME_COUNT_MAX = 10
+    NORMAL_ROLL_COUNT_MAX = 20
+
     attr_reader :rolls
 
     def initialize
@@ -13,29 +17,30 @@ class BowlingGame
     def score
         score = 0
         roll_index = 0
-        for frame in 1..10 do
+
+        NORMAL_FRAME_COUNT_MAX.times {
             if strike?(roll_index: roll_index)
-                score += (10 + strike_bonus(roll_index: roll_index))
+                score += (SCORE_MAX_IN_FRAME + strike_bonus(roll_index: roll_index))
                 roll_index += 1
             elsif spare?(roll_index: roll_index)
-                score += (10 + spare_bonus(roll_index: roll_index))
+                score += (SCORE_MAX_IN_FRAME + spare_bonus(roll_index: roll_index))
                 roll_index += 2
             else
                 score += (@rolls[roll_index] + @rolls[roll_index + 1])
                 roll_index += 2
             end
-        end
+        }
         return score
     end
 
     private
 
     def strike?(roll_index:)
-        return 10 == @rolls[roll_index]
+        return SCORE_MAX_IN_FRAME == @rolls[roll_index]
     end
 
     def spare?(roll_index:)
-        return 10 == (@rolls[roll_index] + @rolls[roll_index + 1])
+        return SCORE_MAX_IN_FRAME == (@rolls[roll_index] + @rolls[roll_index + 1])
     end
 
     def strike_bonus(roll_index:)
--- ruby/spec/bowling_180619_spec.rb
+++ ruby/spec/bowling_180619_spec.rb
@@ -4,19 +4,19 @@ describe 'bowling game 180619' do
     
     it 'all strike' do
         game = BowlingGame.new
-        12.times { game.roll(pins: 10) }
+        (BowlingGame::NORMAL_FRAME_COUNT_MAX + 2).times { game.roll(pins: BowlingGame::SCORE_MAX_IN_FRAME) }
         expect(game.score).to eq(300)
     end
 
     it 'all spare' do
         game = BowlingGame.new
-        21.times { game.roll(pins: 5) }
+        (BowlingGame::NORMAL_ROLL_COUNT_MAX + 1).times { game.roll(pins: 5) }
         expect(game.score).to eq(150)
     end
 
     it 'all nines' do
         game = BowlingGame.new
-        10.times {
+        BowlingGame::NORMAL_FRAME_COUNT_MAX.times {
             game.roll(pins: 9)
             game.roll(pins: 0)
         }
