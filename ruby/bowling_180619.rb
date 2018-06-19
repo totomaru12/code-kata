05aefdcba0bcaea0936597ce611eed3e930ae344
t.oto <t.oto12.dev@gmail.com>
2018-06-19 19:03:24 +0900

    practice ruby bowling 18/06/19

---
+++ ruby/bowling_180619.rb
@@ -0,0 +1,49 @@
+class BowlingGame
+
+    attr_reader :rolls
+
+    def initialize
+        @rolls = []
+    end
+
+    def roll(pins:)
+        @rolls.push(pins)
+    end
+
+    def score
+        score = 0
+        roll_index = 0
+        for frame in 1..10 do
+            if strike?(roll_index: roll_index)
+                score += (10 + strike_bonus(roll_index: roll_index))
+                roll_index += 1
+            elsif spare?(roll_index: roll_index)
+                score += (10 + spare_bonus(roll_index: roll_index))
+                roll_index += 2
+            else
+                score += (@rolls[roll_index] + @rolls[roll_index + 1])
+                roll_index += 2
+            end
+        end
+        return score
+    end
+
+    private
+
+    def strike?(roll_index:)
+        return 10 == @rolls[roll_index]
+    end
+
+    def spare?(roll_index:)
+        return 10 == (@rolls[roll_index] + @rolls[roll_index + 1])
+    end
+
+    def strike_bonus(roll_index:)
+        return @rolls[roll_index + 1] + @rolls[roll_index + 2]
+    end
+
+    def spare_bonus(roll_index:)
+        return @rolls[roll_index + 2]
+    end
+
+end
