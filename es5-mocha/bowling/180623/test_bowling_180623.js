6c001924d161b28b665ceef7c5b842edce482b87
t.oto <t.oto12.dev@gmail.com>
2018-06-23 13:39:15 +0900

    create es5-jasmine and es5-mocha env

---
+++ es5-mocha/bowling/180623/test_bowling_180623.js
@@ -0,0 +1,35 @@
+var expect = require("chai").expect;
+var Bowling = require("./bowling_180623");
+
+describe("Bowling Test", function () {
+
+    Bowling.prototype.rollMany = function (times, pins) {
+        for (let i = 0; i < times; i++) {
+            this.roll(pins);
+        }
+    }
+
+    var bowling = null;
+    beforeEach(function() {
+        bowling = new Bowling();
+    })
+
+    it('all strike', function () {
+        bowling.rollMany(12, 10);
+        expect(bowling.score()).to.be.equal(300);
+    })
+
+    it('all spare', function() {
+        bowling.rollMany(21, 5);
+        expect(bowling.score()).to.be.equal(150);
+    })
+
+    it('all nine', function() {
+        for (var i = 0; i < 10; i++) {
+            bowling.roll(9);
+            bowling.roll(0);
+        }
+        expect(bowling.score()).to.be.equal(90);
+    })
+    
+})
