84fb31e85127ceb86ed7486b0f2c6feb091dcbab
t.oto <t.oto12.dev@gmail.com>
2018-07-20 20:03:18 +0900

    added chai test framework

--- es6/180720_fizzbuzz/test_fizzbuzz_180720.js
+++ es6/180720_fizzbuzz/test_fizzbuzz_180720.js
@@ -1,4 +1,5 @@
 import FizzBuzz from  './fizzbuzz_180720';
+import { expect } from 'chai';
 
 const assert = require('assert');
 const fs = require('fs');
@@ -8,8 +9,12 @@ describe('test fizzbuzz 18/07/20', function() {
     it('all strikes', () => {
         let f = new FizzBuzz();
         const answerFilePath = __dirname + "/fizz_buzz_answer.txt";
-        const answer = fs.readFileSync(answerFilePath);
-        assert.equal(answer, f.getAll());
+        const answer = fs.readFileSync(answerFilePath, 'utf-8');
+        const result = f.getAll();
+
+        // These are test code practicies.
+        assert.equal(answer, result);
+        expect(answer).to.equal(result);
     })
-    
+
 })
