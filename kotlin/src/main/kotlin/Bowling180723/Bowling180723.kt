c99ccb56efce1bdff85d4daef2832672455d3935
t.oto <t.oto12.dev@gmail.com>
2018-07-23 12:51:49 +0900

    practice 18/07/23 kotlin bowling

---
+++ kotlin/src/main/kotlin/Bowling180723/Bowling180723.kt
@@ -0,0 +1,51 @@
+package Bowling180723
+
+class Bowling180723 {
+
+    private val rolls: MutableList<Int> = mutableListOf()
+
+    val NORMAL_FRAME_MAX = 10
+    val SCORE_MAX_IN_FRAME = 10
+
+    fun roll(pins: Int) {
+        rolls.add(pins)
+    }
+
+    val score : Int
+        get() {
+            var score = 0
+            var rollIndex = 0
+            (1..NORMAL_FRAME_MAX).forEach {
+                when {
+                    isStrike(rollIndex) -> {
+                        score += scoreStrike(rollIndex)
+                        rollIndex += 1
+                    }
+                    isSpare(rollIndex) -> {
+                        score += scoreSpare(rollIndex)
+                        rollIndex += 2
+                    }
+                    else -> {
+                        score += scoreNormal(rollIndex)
+                        rollIndex += 2
+                    }
+                }
+            }
+            return score
+        }
+
+    private fun isStrike(rollIndex: Int)
+        = (SCORE_MAX_IN_FRAME == rolls[rollIndex])
+    
+    private fun isSpare(rollIndex: Int)
+        = (SCORE_MAX_IN_FRAME == (rolls[rollIndex] + rolls[rollIndex + 1]))
+
+    private fun scoreStrike(rollIndex: Int)
+        = SCORE_MAX_IN_FRAME + rolls[rollIndex + 1] + rolls[rollIndex + 2]
+
+    private fun scoreSpare(rollIndex: Int)
+        = SCORE_MAX_IN_FRAME + rolls[rollIndex + 2]
+
+    private fun scoreNormal(rollIndex: Int)
+        = rolls[rollIndex] + rolls[rollIndex + 1]
+}
