a344d93920b88932f8700f55eebe9aaf07bd07cb
t.oto <t.oto12.dev@gmail.com>
2018-07-30 12:49:50 +0900

    practice typescript bowling 17/07/30

---
+++ ts/180730_bowling/test_bowling_180730.ts
@@ -0,0 +1,46 @@
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import { Bowling } from "./bowling_180730";
+
+describe('180730 bowling test', () => {
+
+  it ('all strikes', () => {
+    let b = new Bowling();
+    for (let i = 0; i < Bowling.NORMAL_FRAME_MAX; i++) {
+      b.roll(Bowling.SCORE_MAX_IN_FRAME);
+    }
+    b.roll(Bowling.SCORE_MAX_IN_FRAME);
+    b.roll(Bowling.SCORE_MAX_IN_FRAME);
+    expect(b.score()).equal(300);
+  });
+
+  it ('all spares with 5 pins', () => {
+    let b = new Bowling();
+    for (let i = 0; i < Bowling.NORMAL_FRAME_MAX; i++) {
+      b.roll(5);
+      b.roll(5);
+    }
+    b.roll(5);
+    expect(b.score()).equal(150);
+  })
+
+  it ('all nine and zero pairs', () => {
+    let b = new Bowling();
+    for (let i = 0; i < Bowling.NORMAL_FRAME_MAX; i++) {
+      b.roll(9);
+      b.roll(0);
+    }
+    expect(b.score()).equal(90);
+  });
+
+  it ('all spares with 9 and 0 pairs', () => {
+    let b = new Bowling();
+    for (let i = 0; i < Bowling.NORMAL_FRAME_MAX; i++) {
+      b.roll(9);
+      b.roll(1);
+    }
+    b.roll(9);
+    expect(b.score()).equal(190);
+  });
+
+});
