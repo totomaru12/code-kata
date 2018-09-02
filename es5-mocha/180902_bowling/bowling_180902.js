1a84aff1e8039de1b5fdbb87ec450dd680c3105b
t.oto <t.oto12.dev@gmail.com>
2018-09-02 20:55:16 +0900

    [WIP] added bowling constant definition

--- es5-mocha/180902_bowling/bowling_180902.js
+++ es5-mocha/180902_bowling/bowling_180902.js
@@ -4,6 +4,14 @@ function bowling() {
 
 }
 
+bowling.prototype.NORMAL_FRAME_MAX = function() {
+    return 10;
+}
+
+bowling.prototype.SCORE_MAX_IN_FRAME = function() {
+    return 10;
+}
+
 bowling.prototype.roll = function(pins) {
 
 }
--- es5-mocha/180902_bowling/test_bowling_180902.js
+++ es5-mocha/180902_bowling/test_bowling_180902.js
@@ -9,13 +9,12 @@ describe("Bowling 18/09/02", function() {
     })
 
     it("all strikes", function() {
-        for (var i = 0; i < 10; i++) {
-            b.roll(10);
+        for (var i = 0; i < b.NORMAL_FRAME_MAX(); i++) {
+            b.roll(b.SCORE_MAX_IN_FRAME());
         }
-        b.roll(10);
-        b.roll(10);
+        b.roll(b.SCORE_MAX_IN_FRAME());
+        b.roll(b.SCORE_MAX_IN_FRAME());
         expect(b.score()).to.be.equal(300);
     })
-
 
 })
