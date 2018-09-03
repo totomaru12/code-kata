5508434afe8d65466712858d81060fdc67a0d3cd
t.oto <t.oto12.dev@gmail.com>
2018-09-03 12:46:37 +0900

    practice 18/09/03 bowling es6

---
+++ es6/180903_bowling/test_bowling_180903.js
@@ -0,0 +1,53 @@
+import Bowling from './bowling_180903';
+import { expect } from 'chai';
+
+describe('test bowling 18/09/03', () => {
+
+    it('all strikes', () => {
+        let b = new Bowling();
+        for (let i = 0; i < b.NORMAL_FRAME_MAX(); i++) {
+            b.roll(b.SCORE_MAX_IN_FRAME());
+        }
+        b.roll(b.SCORE_MAX_IN_FRAME());
+        b.roll(b.SCORE_MAX_IN_FRAME());
+        expect(b.score()).to.equal(300);
+    })
+
+    it('all spares with 9 and 1', () => {
+        let b = new Bowling();
+        for (let i = 0; i < b.NORMAL_FRAME_MAX(); i++) {
+            b.roll(9);
+            b.roll(1);
+        }
+        b.roll(9);
+        expect(b.score()).to.equal(190);
+    })
+
+    it('all frames with 8 and 1', () => {
+        let b = new Bowling();
+        for (let i = 0; i < b.NORMAL_FRAME_MAX(); i++) {
+            b.roll(8);
+            b.roll(1);
+        }
+        expect(b.score()).to.equal(90);
+    })
+
+    it('all frames with 9 and 0', () => {
+        let b = new Bowling();
+        for (let i = 0; i < b.NORMAL_FRAME_MAX(); i++) {
+            b.roll(9);
+            b.roll(0);
+        }
+        expect(b.score()).to.equal(90);
+    })
+
+    it('all frames with 0 and 9', () => {
+        let b = new Bowling();
+        for (let i = 0; i < b.NORMAL_FRAME_MAX(); i++) {
+            b.roll(0);
+            b.roll(9);
+        }
+        expect(b.score()).to.equal(90);
+    })
+
+})
