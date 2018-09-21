2bae8e0e99ba3f985752eec446e110557c6bf273
t.oto <t.oto12.dev@gmail.com>
2018-09-21 22:21:55 +0900

    [WIP] added bowling logic

--- cs/cs-kata/Bowling180921.cs
+++ cs/cs-kata/Bowling180921.cs
@@ -4,15 +4,57 @@ public class Bowling180921
 {
     public static readonly int NORMAL_FRAME_MAX = 10;
     public static readonly int SCORE_MAX_IN_FRAME = 10;
-    
+
+    private List<int> rolls = new List<int>();
+
     public void Roll(int pins)
     {
-
+        this.rolls.Add(pins);
     }
 
     public int Score()
     {
-        return 300;
+        int score = 0;
+        int rollIndex = 0;
+        for (int frame = 0; frame < NORMAL_FRAME_MAX; frame ++)
+        {
+            if (this.isStrike(rollIndex)) {
+                score += this.scoreStrike(rollIndex);
+                rollIndex += 1;
+            } else if (this.isSpare(rollIndex)) {
+                score += this.scoreSpare(rollIndex);
+                rollIndex += 2;
+            } else {
+                score += this.scoreNormal(rollIndex);
+                rollIndex += 2;
+            }
+        }
+        return score;
+    }
+
+    public bool isStrike(int rollIndex)
+    {
+        return SCORE_MAX_IN_FRAME == this.rolls[rollIndex];
+    }
+
+    public bool isSpare(int rollIndex)
+    {
+        return SCORE_MAX_IN_FRAME == this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+    }
+
+    public int scoreStrike(int rollIndex)
+    {
+        return SCORE_MAX_IN_FRAME + this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+    }
+
+    public int scoreSpare(int rollIndex)
+    {
+        return SCORE_MAX_IN_FRAME + this.rolls[rollIndex + 2];
+    }
+
+    public int scoreNormal(int rollIndex)
+    {
+        return this.rolls[rollIndex] + this.rolls[rollIndex];
     }
 
 }
