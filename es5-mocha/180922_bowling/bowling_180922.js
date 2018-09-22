9ab6967cf35631cf168b4e48858436c454c515b1
t.oto <t.oto12.dev@gmail.com>
2018-09-22 14:09:59 +0900

    [WIP] minimum test and bowling implementation

---
+++ es5-mocha/180922_bowling/bowling_180922.js
@@ -0,0 +1,15 @@
+"use strict"
+
+function bowling() {
+    
+}
+
+bowling.prototype.roll = function(pins) {
+
+}
+
+bowling.prototype.score = function() {
+    return 300;
+}
+
+module.exports = bowling;
