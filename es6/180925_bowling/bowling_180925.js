a6185c58173adeaac9e48e21bd66778b3798836c
t.oto <t.oto12.dev@gmail.com>
2018-09-25 22:30:24 +0900

    [WIP] added bowling logic

--- es6/180925_bowling/bowling_180925.js
+++ es6/180925_bowling/bowling_180925.js
@@ -1,5 +1,9 @@
 export default class Bowling {
 
+    constructor() {
+        this.rolls = [];
+    }
+
     NORMAL_FRAME_MAX() {
         return 10;
     }
@@ -8,12 +12,47 @@ export default class Bowling {
         return 10;
         
     }
-    
-    roll(pins) {
 
+    roll(pins) {
+        this.rolls.push(pins);
     }
 
     score() {
-        return 300;
+        let score = 0;
+        let rollIndex = 0;
+        for (let frame = 0; frame < this.NORMAL_FRAME_MAX(); frame ++) {
+            if (this.isStrike(rollIndex)) {
+                score += this.scoreStrike(rollIndex);
+                rollIndex += 1;
+            } else if (this.isSpare(rollIndex)) {
+                score += this.scoreSpare(rollIndex);
+                rollIndex += 2;
+            } else {
+                score += this.scoreNormal(rollIndex);
+                rollIndex += 2;
+            }
+        }
+        return score;
     }
+
+    isStrike(rollIndex) {
+        return this.SCORE_MAX_IN_FRAME() == this.rolls[rollIndex];
+    }
+
+    isSpare(rollIndex) {
+        return this.SCORE_MAX_IN_FRAME() == this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+    }
+
+    scoreStrike(rollIndex) {
+        return this.SCORE_MAX_IN_FRAME() + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+    }
+
+    scoreSpare(rollIndex) {
+        return this.SCORE_MAX_IN_FRAME() + this.rolls[rollIndex + 2];
+    }
+
+    scoreNormal(rollIndex) {
+        return this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+    }
+
 }
