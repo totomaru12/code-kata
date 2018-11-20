64b0f05051c8cf4975de3ec22d40fe351bb4ce6a
t.oto <t.oto12.dev@gmail.com>
2018-11-20 12:48:50 +0900

    removed scala extra files

--- scala/src/test/scala/HelloTest.scala
+++
@@ -1,8 +0,0 @@
-import org.scalatest._
-
-class HelloTest extends FlatSpec with Matchers {
-    "Hello.getMessage" should "return expected message" in {
-    val message = Hello.getMessage()
-    message shouldBe "Hello world!"
-  }
-}
